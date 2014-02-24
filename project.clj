a5e746a1fec3360c4370e9537775d0039c99644b
Andy Buntine <bunts@hhd.com.au>
2014-02-24 17:24:06 +1100

    Updated config. Gonna' get this badboy up and running again

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject mam "1.0.0-SNAPSHOT"
   :description "A sadistic, pro-death text-based adventure for children"
-  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
-                 [org.clojure/clojure-contrib "1.2.0-beta1"]]
+  :license "Eclipse Public License 1.0"
+  :url "http://github.com/buntine/Medieval-Alien-Massacre"
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :main mam.core)
